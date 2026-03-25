fd05982b8e3a626919b84116f2d2379618b140eb
blanckaertgabriel3-sketch <blanckaertgabriel3@gmail.com>
2026-03-25 10:28:32 +0100

    product class

--- classes/Product.js
+++ classes/Product.js
@@ -1,5 +1,23 @@
 class Product {
-	constructor() {
-		
+	constructor(name, price) {
+		this.name = name;
+		this.price = price;
+		this.quantity = 0;
+		this.category = null; //hot, cold, soft
+		this.types = ["cold", "hot", "soft"];
 	}
+	//quantity
+	addQuantity(quantity) {
+		this.quantity += quantity;
+	}
+	removeQuantity(quantity) {
+		this.quantity -= quantity;
+	}
+	//category
+	setCategory(type) {
+		if (this.types.includes(type)) {
+			this.category = type;
+		}
+	}
+
 }
